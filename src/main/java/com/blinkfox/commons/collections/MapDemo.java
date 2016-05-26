e00388abc14ad1c9dc929a189756d02b92803802
blinkfox <chenjiayin1990@163.com>
2016-05-26 23:50:22 +0800

    新增Apache Commons Collections包和map相关的使用demo

---
+++ src/main/java/com/blinkfox/commons/collections/MapDemo.java
@@ -0,0 +1,84 @@
+package com.blinkfox.commons.collections;
+
+import org.apache.commons.collections4.BidiMap;
+import org.apache.commons.collections4.IterableMap;
+import org.apache.commons.collections4.MapIterator;
+import org.apache.commons.collections4.OrderedMap;
+import org.apache.commons.collections4.bidimap.TreeBidiMap;
+import org.apache.commons.collections4.map.HashedMap;
+import org.apache.commons.collections4.map.LinkedMap;
+
+import java.util.Map;
+
+/**
+ * Apache Commons Collections包map相关的使用测试
+ * Created by blinkfox on 16-5-26.
+ */
+public class MapDemo {
+
+    private static void buildMap(Map map) {
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+    }
+
+    /**
+     * 遍历map
+     *
+     * @param map
+     * @param mapName
+     */
+    private static void loopMap(IterableMap map, String mapName) {
+        // 遍历map
+        MapIterator it = map.mapIterator();
+        while (it.hasNext()) {
+            Object key = it.next();
+            Object value = it.getValue();
+            System.out.println(mapName + " key:" + key + ", value: " + value);
+        }
+    }
+
+    private static void iterableMapTest() {
+        IterableMap iterMap = new HashedMap<>();
+        buildMap(iterMap);
+        loopMap(iterMap, "iterableMap");
+    }
+
+    private static void linkedMapTest() {
+        OrderedMap<String, Object> orderMap = new LinkedMap<String, Object>();
+        orderMap.put("one", "1");
+        orderMap.put("two", "2");
+        orderMap.put("three", "3");
+        orderMap.put("four", "4");
+
+        // 获取map中相应的值
+        System.out.println("LinkedMap firstKey:" + orderMap.firstKey());
+        System.out.println("LinkedMap previous key:" + orderMap.previousKey("four"));
+        System.out.println("LinkedMap next key:" + orderMap.nextKey("two"));
+        System.out.println("LinkedMap last key:" + orderMap.lastKey());
+        System.out.println("LinkedMap map Size:" + orderMap.size());
+    }
+
+    private static void bidiMapTest() {
+        BidiMap bidiMap = new TreeBidiMap();
+        bidiMap.put("one", "1");
+        bidiMap.put("two", "2");
+        bidiMap.put("three", "3");
+        bidiMap.put("san", "3");    // 有相同值的，只有最后一个生效
+
+        // 获取map中相应的值
+        System.out.println("BidiMap getKey:" + bidiMap.getKey("2"));
+        System.out.println("BidiMap getMoreSameKey:" + bidiMap.getKey("3"));
+
+        bidiMap.removeValue("3");
+        System.out.println("BidiMap getMoreSameKey2:" + bidiMap.getKey("3"));
+    }
+
+    public static void main(String[] args) {
+        iterableMapTest();
+        linkedMapTest();
+        bidiMapTest();
+    }
+
+}
---
+++ src/main/java/com/blinkfox/commons/lang/StringTest.java
@@ -0,0 +1,7 @@
+package com.blinkfox.commons.lang;
+
+/**
+ * Created by blinkfox on 16-5-26.
+ */
+public class StringTest {
+}
