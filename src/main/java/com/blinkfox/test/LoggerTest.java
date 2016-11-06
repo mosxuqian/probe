f594e08b70379a3420452ae65c79007e1078063f
blinkfox <chenjiayin1990@163.com>
2016-11-06 12:51:33 +0800

    新增了基于JDK Logging自定义的日志封装

--- src/main/java/com/blinkfox/test/LoggerTest.java
+++ src/main/java/com/blinkfox/test/LoggerTest.java
@@ -1,5 +1,7 @@
 package com.blinkfox.test;
 
+import com.blinkfox.utils.Log;
+
 import java.util.logging.Logger;
 
 /**
@@ -8,12 +10,20 @@ import java.util.logging.Logger;
  */
 public class LoggerTest {
 
+    private static final Log logger = Log.get(LoggerTest.class);
+
     public static void main(String[] args) {
         Logger log = Logger.getLogger(LoggerTest.class.getName());
         log.severe("这是 severe 级别的日志");
         log.warning("这是 warning 级别的日志");
         log.info("这是 info 级别的日志");
         log.config("这是 config 级别的日志");
+
+        log.info("-----------以下是LogHelper的使用-----------");
+        logger.info("info日志输出");
+        logger.warn("warn级别的消息");
+        logger.warn("error级别的消息");
+        logger.error("error信息输出", new RuntimeException());
     }
 
 }
