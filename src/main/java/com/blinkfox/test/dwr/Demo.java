ba468b759bd3181177c20b8914a17cde702db5cb
blinkfox <chenjiayin1990@163.com>
2016-11-23 00:12:28 +0800

    修改dwr的使用方式，改为更详细的配置方式

--- src/main/java/com/blinkfox/test/dwr/Demo.java
+++ src/main/java/com/blinkfox/test/dwr/Demo.java
@@ -1,5 +1,7 @@
 package com.blinkfox.test.dwr;
 
+import com.blinkfox.zealot.helpers.StringHelper;
+
 /**
  * dwr demo示例类
  * Created by blinkfox on 2016/11/21.
@@ -13,6 +15,9 @@ public class Demo {
      */
     public String sayHello(String word) {
         System.out.println("-----the word is:" + word);
+        if (StringHelper.isBlank(word)) {
+            throw new RuntimeException("你输入的关键字为空");
+        }
         return new StringBuilder("Hello ").append(word).append("!").toString();
     }
 
