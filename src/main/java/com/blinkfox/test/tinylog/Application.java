6a4684077c340f4ad236d6b74645ccc7a0dd2a49
blinkfox <chenjiayin1990@163.com>
2016-12-08 00:21:45 +0800

    完善了tinylog的使用示例

--- src/main/java/com/blinkfox/test/tinylog/Application.java
+++ src/main/java/com/blinkfox/test/tinylog/Application.java
@@ -1,6 +1,9 @@
 package com.blinkfox.test.tinylog;
 
 import org.pmw.tinylog.Logger;
+import org.pmw.tinylog.LoggingContext;
+
+import java.util.Date;
 
 /**
  * tinylog测试类
@@ -9,10 +12,25 @@ import org.pmw.tinylog.Logger;
 public class Application {
 
     public static void main(String[] args) {
+        Logger.trace("Hello trace");
         Logger.debug("Hello debug");
         Logger.info("Hello info");
         Logger.warn("Hello warn");
         Logger.error("Hello error");
+
+        Date date = new Date();
+        Logger.info(date);
+
+        Logger.info("{} + {} = 3", 1, 2);
+
+        String a = null;
+        try {
+            a.toString();
+        } catch (NullPointerException e) {
+            Logger.error(e, "a is {}", a);
+        }
+
+        LoggingContext.put("userName", "张三");
     }
 
 }
