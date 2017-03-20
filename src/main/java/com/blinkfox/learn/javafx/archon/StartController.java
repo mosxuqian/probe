ea3487368d3415924e4abe0e1a7a58eab643bbf6
blinkfox <chenjiayin1990@163.com>
2017-03-20 17:34:11 +0800

    新增start中上一步、下一步的切换功能

--- src/main/java/com/blinkfox/learn/javafx/archon/StartController.java
+++ src/main/java/com/blinkfox/learn/javafx/archon/StartController.java
@@ -1,11 +1,71 @@
 package com.blinkfox.learn.javafx.archon;
 
+import com.blinkfox.learn.javafx.archon.consts.Constant;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 /**
  * start界面的控制器.
  * Created by blinkfox on 2017-03-20.
  */
 public class StartController {
 
+    /* 上一步、下一步和隐藏文本字段的控件 */
+    @FXML
+    private Button prevStepBtn;
+    @FXML
+    private Button nextStepBtn;
+    @FXML
+    private TextField hideStepField;
 
+    /* git账户、默认仓库和开始使用的三个label */
+    @FXML
+    private AnchorPane gitAccountPane;
+    @FXML
+    private AnchorPane defaultRepoPane;
+    @FXML
+    private AnchorPane startUsePane;
+
+    /**
+     * 下一步的相关操作.
+     */
+    @FXML
+    private void nextStepAction() {
+        String step = hideStepField.getText();
+        if (Constant.STEP_ONE.equals(step)) {
+            gitAccountPane.setVisible(false);
+            defaultRepoPane.setVisible(true);
+            prevStepBtn.setVisible(true);
+            hideStepField.setText(Constant.STEP_TWO);
+        } else if (Constant.STEP_TWO.equals(step)) {
+            defaultRepoPane.setVisible(false);
+            startUsePane.setVisible(true);
+            nextStepBtn.setText("完成");
+            hideStepField.setText(Constant.STEP_THREE_INIT);
+        }
+    }
+
+    /**
+     * 上一步相关操作.
+     */
+    @FXML
+    private void prevStepAction() {
+        String step = hideStepField.getText();
+        if (Constant.STEP_TWO.equals(step)) {
+            defaultRepoPane.setVisible(false);
+            gitAccountPane.setVisible(true);
+            prevStepBtn.setVisible(false);
+            hideStepField.setText(Constant.STEP_ONE);
+        } else if (Constant.STEP_THREE_INIT.equals(step)
+                || Constant.STEP_THREE_CLONE.equals(step)
+                || Constant.STEP_THREE_OPEN.equals(step)) {
+            startUsePane.setVisible(false);
+            defaultRepoPane.setVisible(true);
+            nextStepBtn.setText("下一步");
+            hideStepField.setText(Constant.STEP_TWO);
+        }
+    }
 
 }
---
+++ src/main/java/com/blinkfox/learn/javafx/archon/consts/Constant.java
@@ -0,0 +1,34 @@
+package com.blinkfox.learn.javafx.archon.consts;
+
+/**
+ * archon的常量类
+ * Created by blinkfox on 2017-03-20.
+ */
+public final class Constant {
+
+    /* start第一步 */
+    public static final String STEP_ONE = "1";
+
+    /* start第二步 */
+    public static final String STEP_TWO = "2";
+
+    /* start第三步 */
+    public static final String STEP_THREE = "3";
+
+    /* start第三步，初始化创建 */
+    public static final String STEP_THREE_INIT = "3-1";
+
+    /* start第三步，克隆创建 */
+    public static final String STEP_THREE_CLONE = "3-2";
+
+    /* start第三步，打开已有 */
+    public static final String STEP_THREE_OPEN = "3-3";
+
+    /**
+     * 私有构造方法.
+     */
+    private Constant() {
+        super();
+    }
+
+}
