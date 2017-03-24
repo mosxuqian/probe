6f31ece216d6deab1cfc52b6734f91779560dafd
blinkfox <chenjiayin1990@163.com>
2017-03-25 00:31:06 +0800

    新增了FontAwesomeFx的使用示例

---
+++ src/main/java/com/blinkfox/learn/javafx/others/FontAwesomeFxTest.java
@@ -0,0 +1,18 @@
+package com.blinkfox.learn.javafx.others;/**
+ * Created by blinkfox on 2017/3/24.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FontAwesomeFxTest extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
