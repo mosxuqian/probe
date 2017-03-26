dd685ab8e31351b729bc8577ef69c0a239a52fe3
blinkfox <chenjiayin1990@163.com>
2017-03-26 16:11:48 +0800

    新增TreeView示例的纯右键点击功能

--- src/main/java/com/blinkfox/learn/javafx/ui/tree/TreeViewSample.java
+++ src/main/java/com/blinkfox/learn/javafx/ui/tree/TreeViewSample.java
@@ -1,14 +1,18 @@
 package com.blinkfox.learn.javafx.ui.tree;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.pmw.tinylog.Logger;
 
 /**
  * JavaFx TreeView使用示例.
@@ -32,7 +36,19 @@ public class TreeViewSample extends Application {
             TreeItem<String> item = new TreeItem<>("文件" + (i + 1));
             rootItem.getChildren().add(item);
         }
+
+        // 右键菜单
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem updateItem = new MenuItem("更新");
+        MenuItem commitItem = new MenuItem("提交");
+        updateItem.setOnAction(event -> Logger.info("开始更新了..."));
+        commitItem.setOnAction(event -> Logger.info("开始提交数据了..."));
+        ObservableList<MenuItem> items =  contextMenu.getItems();
+        items.add(updateItem);
+        items.add(commitItem);
+
         TreeView<String> treeView = new TreeView<>(rootItem);
+        treeView.setContextMenu(contextMenu);
 
         StackPane root = new StackPane(treeView);
         primaryStage.setScene(new Scene(root, 300, 250));
