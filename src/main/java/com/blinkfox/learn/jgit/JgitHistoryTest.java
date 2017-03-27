e8e0be5c6c0fd4b40f12ab9fa25a206fe1b4f644
blinkfox <chenjiayin1990@163.com>
2017-03-28 01:11:11 +0800

    新增jGit获取仓库历史记录的功能

---
+++ src/main/java/com/blinkfox/learn/jgit/JgitHistoryTest.java
@@ -0,0 +1,79 @@
+package com.blinkfox.learn.jgit;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.pmw.tinylog.Logger;
+
+/**
+ * jGit获取历史记录的测试示例.
+ * Created by blinkfox on 2017/3/28.
+ */
+public class JgitHistoryTest {
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    /**
+     * 私有构造方法.
+     */
+    private JgitHistoryTest() {
+        super();
+    }
+
+    /**
+     * main方法.
+     * @param args 数组参数
+     */
+    public static void main(String[] args) throws Exception {
+        gitLog();
+    }
+
+    /**
+     * 使用revWalk方法.
+     */
+    private static void gitLog() throws Exception {
+        Git git = Git.open(new File("/Users/blinkfox/Documents/dev/gitrepo/probe"));
+        Iterable<RevCommit> commits = git.log().call();
+        for (RevCommit commit: commits) {
+            // 处理日期
+            int time = commit.getCommitTime();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(time);
+            String timeStr = sdf.format(calendar.getTime());
+
+            Logger.info("提交信息：{}；作者：{}<{}>；时间：{}；提交：{}", commit.getFullMessage(),
+                    commit.getAuthorIdent().getName(),commit.getAuthorIdent().getEmailAddress(),
+                    timeStr, commit.getId());
+        }
+        git.close();
+    }
+
+    /**
+     * 使用revWalk方法.
+     */
+    private static void revWalk() throws Exception {
+        Repository repo;
+        RevWalk revWalk;
+        ObjectId commitId;
+        try (Git git = Git.open(new File("/Users/blinkfox/Documents/dev/gitrepo/carrier"))) {
+            repo = git.getRepository();
+        }
+        revWalk = new RevWalk(repo);
+
+        commitId = repo.resolve("refs/heads/master");
+        revWalk.markStart(revWalk.parseCommit(commitId));
+        for (RevCommit commit : revWalk) {
+            Logger.info("提交信息：{}", commit.getFullMessage());
+        }
+    }
+
+}
