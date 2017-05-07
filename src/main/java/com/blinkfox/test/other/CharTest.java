ddd143001b0736fdc0d1b337a5f894661c4b2974
blinkfox <chenjiayin1990@163.com>
2017-05-07 20:13:11 +0800

    新增了将长整型数据转换成固定长度的，位数不够时后面补0的字符串

--- src/main/java/com/blinkfox/test/other/CharTest.java
+++ src/main/java/com/blinkfox/test/other/CharTest.java
@@ -1,6 +1,9 @@
 package com.blinkfox.test.other;
 
 import java.io.UnsupportedEncodingException;
+import java.text.DecimalFormat;
+
+import com.blinkfox.test.sorts.others.PhoneNumberSort;
 import org.pmw.tinylog.Logger;
 
 /**
@@ -9,10 +12,20 @@ import org.pmw.tinylog.Logger;
  */
 public class CharTest {
 
+    private static final String STR_FORMAT = "00000000000";
+
+    private static final DecimalFormat df = new DecimalFormat(STR_FORMAT);
+
     public static void main(String[] args) throws UnsupportedEncodingException {
         String msg = "13880545209";
         int len = msg.getBytes("UTF-8").length;
         Logger.info("len:{}", len);
+
+        Logger.info("手机号1:{}", PhoneNumberSort.toNumStr(13801L));
+        Logger.info("手机号2:{}", PhoneNumberSort.toNumStr(13880523L));
+        Logger.info("手机号3:{}", PhoneNumberSort.toNumStr(1380003L));
+        Logger.info("手机号4:{}", PhoneNumberSort.toNumStr(1380103213L));
+        Logger.info("手机号5:{}", PhoneNumberSort.toNumStr(13801032135L));
     }
 
 }
--- src/main/java/com/blinkfox/test/sorts/others/PhoneNumberSort.java
+++ src/main/java/com/blinkfox/test/sorts/others/PhoneNumberSort.java
@@ -60,14 +60,14 @@ public class PhoneNumberSort {
         try {
             is = new FileInputStream(INPUT_FILE_NAME);
             reader = new BufferedReader(new InputStreamReader(is));
-            List<String> phoneNums = new ArrayList<String>();
+            List<Long> phoneNums = new ArrayList<Long>();
 
             // 读取代码行数，并拆分成不同的数据输出到多个临时文件中
             int currLines = 0;
             String line = null;
             while ((line = reader.readLine()) != null) {
                 currLines += 1;
-                phoneNums.add(line);
+                phoneNums.add(Long.parseLong(line));
 
                 // 如果当前读取的行数大于了最大行数，则排序目前的集合，且输出到临时文件中
                 if (currLines >= MAX_LINE) {
@@ -92,7 +92,7 @@ public class PhoneNumberSort {
      */
     private static void mergeFiles() {
         List<Reader> readers = new ArrayList<Reader>();
-        Map<String, BufferedReader> readerMap = new HashMap<String, BufferedReader>();
+        Map<Long, BufferedReader> readerMap = new HashMap<Long, BufferedReader>();
 
         FileReader fileReader = null;
         FileWriter fileWriter = null;
@@ -108,24 +108,25 @@ public class PhoneNumberSort {
                 BufferedReader reader = new BufferedReader(fileReader);
                 String phoneNum = reader.readLine();
                 if (phoneNum != null) {
-                    readerMap.put(phoneNum, reader);
+                    readerMap.put(Long.parseLong(phoneNum), reader);
                 }
             }
 
             // 采用归并排序的思想,一直循环排序直到各个文件没有了电话号码为止
             fileWriter = new FileWriter(OUTPUT_FILE_NAME);
             writer = new BufferedWriter(fileWriter);
-            List<String> phoneNums = new LinkedList<String>(readerMap.keySet());
+            List<Long> phoneNums = new LinkedList<Long>(readerMap.keySet());
             while (readerMap.size() > 0) {
                 // 排序，将最大的手机号从phoneNums集合中移除，并写入到输出文件中
-                Collections.sort(phoneNums, StringComparator.getInstance());
-                String phoneNum = phoneNums.remove(0);
-                writer.append(phoneNum).append('\n');
+                Collections.sort(phoneNums, LongComparator.getInstance());
+                Long phoneNum = phoneNums.remove(0);
+                writer.append(toNumStr(phoneNum)).append('\n');
 
                 // 从readerMap中移除刚才得出的最大手机号,并从该reader中读取下一个手机号存到List和Map中，继续循环写入
                 BufferedReader reader = readerMap.remove(phoneNum);
-                String nextPhoneNum = reader.readLine();
-                if (nextPhoneNum != null) {
+                String nextPhoneNumStr = reader.readLine();
+                if (nextPhoneNumStr != null) {
+                    Long nextPhoneNum = Long.parseLong(nextPhoneNumStr);
                     phoneNums.add(nextPhoneNum);
                     readerMap.put(nextPhoneNum, reader);
                 }
@@ -144,9 +145,9 @@ public class PhoneNumberSort {
      * 排序集合数据，并输出到临时文件中文件.
      * @param phoneNums 电话号码集合
      */
-    private static void sortAndOutputFile(List<String> phoneNums) {
+    private static void sortAndOutputFile(List<Long> phoneNums) {
         long startTime = System.currentTimeMillis();
-        Collections.sort(phoneNums, StringComparator.getInstance());
+        Collections.sort(phoneNums, LongComparator.getInstance());
         System.out.println("排序集合数据耗时:" + (System.currentTimeMillis() - startTime) + " ms");
 
         // 输出到临时文件
@@ -168,7 +169,7 @@ public class PhoneNumberSort {
      * @param file 文件
      * @param lines 集合
      */
-    public static void writeLines(File file, List<String> lines) {
+    public static void writeLines(File file, List<Long> lines) {
         if (lines == null || lines.isEmpty()) {
             return;
         }
@@ -178,9 +179,9 @@ public class PhoneNumberSort {
         try {
             fw = new FileWriter(file);
             writer = new BufferedWriter(fw);
-            for (String line: lines) {
+            for (Long line: lines) {
                 if (line != null) {
-                    writer.append(line).append('\n');
+                    writer.append(toNumStr(line)).append('\n');
                 }
             }
         } catch (IOException e) {
@@ -260,6 +261,26 @@ public class PhoneNumberSort {
             System.out.println("关闭输出流出错！");
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 将long型数据转换成电话号码的11位字符串.
+     * @param l 长整型数字
+     * @return 字符串
+     */
+    public static String toNumStr(Long l) {
+        String str = l.toString();
+        int len = 0;
+        if ((len = str.length()) == 11) {
+            return str;
+        }
+
+        // 长度不是11位则在后面补0
+        StringBuilder sb = new StringBuilder(str);
+        for (int i = 0; i < 11 - len; i++) {
+            sb.append("0");
+        }
+        return sb.toString();
     }
 
 }
