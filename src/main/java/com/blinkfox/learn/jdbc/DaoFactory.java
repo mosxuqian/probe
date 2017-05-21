40f1cdc20cfe03e9326e4ac30d031d9f39b11a5b
blinkfox <chenjiayin1990@163.com>
2017-05-21 17:50:06 +0800

    新增数据库访问的工具类和升级mariadb的java-client版本

---
+++ src/main/java/com/blinkfox/learn/jdbc/DaoFactory.java
@@ -0,0 +1,68 @@
+package com.blinkfox.learn.jdbc;
+
+import com.blinkfox.utils.others.PropHelper;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import org.pmw.tinylog.Logger;
+
+/**
+ * 数据库访问空间类.
+ * Created by blinkfox on 2017/5/21.
+ */
+public class DaoFactory {
+
+    /* MariaDB驱动... */
+    private static String driver = "";
+    private static String url = "";
+    private static String user = "";
+    private static String pwd = null;
+
+    /* 获取数据库连接相关的配置属性. */
+    static {
+        Properties props = PropHelper.INSTANCE.loadPropFile("props/config.properties");
+        driver = props.getProperty("driver");
+        url = props.getProperty("url");
+        user = props.getProperty("username");
+        pwd = props.getProperty("password");
+    }
+
+    /**
+     * 获取数据库连接.
+     */
+    public static Connection getConnection() {
+        try {
+            Class.forName(driver);
+            return DriverManager.getConnection(url, user, pwd);
+        } catch (Exception e) {
+            Logger.error(e, "注册数据库，获取数据库连接出错！");
+            return null;
+        }
+    }
+
+    /**
+     * 关闭数据库连接.
+     * @param conn 数据库连接
+     */
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                Logger.error(e, "关闭数据库连接失败！");
+            }
+        }
+    }
+
+    /**
+     * main方法.
+     * @param args 数组参数
+     */
+    public static void main(String[] args) {
+        Connection conn = getConnection();
+        Logger.info("conn:{}", conn);
+        closeConnection(conn);
+    }
+
+}
