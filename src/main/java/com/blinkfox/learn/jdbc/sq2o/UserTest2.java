2bd0aafeb4bcc4fe79935e8f58d5be2d60c5e94f
blinkfox <chenjiayin1990@163.com>
2017-05-25 23:54:01 +0800

    新增使用sq2o查询数据库的简单示例

---
+++ src/main/java/com/blinkfox/learn/jdbc/sq2o/UserTest2.java
@@ -0,0 +1,37 @@
+package com.blinkfox.learn.jdbc.sq2o;
+
+import com.blinkfox.learn.jdbc.dbpool.DataSourceHelper;
+import java.util.List;
+import java.util.Map;
+import org.pmw.tinylog.Logger;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+/**
+ * UserTest2.
+ * Created by blinkfox on 2017/5/25.
+ */
+public class UserTest2 {
+
+    /**
+     * 查询用户信息.
+     */
+    private static void queryUsers() {
+        String sql = "SELECT * FROM user AS u WHERE u.age > ? AND u.nickname LIKE ?";
+        Sql2o sql2o = new Sql2o(DataSourceHelper.getDataSource());
+        try (Connection conn = sql2o.open()) {
+            List<Map<String, Object>> resultMaps = conn.createQueryWithParams(sql, 18, "")
+                    .executeAndFetchTable().asList();
+            Logger.info("查询用户信息成功!, resultMaps:{}", resultMaps);
+        }
+    }
+
+    /**
+     * main方法.
+     * @param args 数组参数
+     */
+    public static void main(String[] args) {
+        queryUsers();
+    }
+
+}
