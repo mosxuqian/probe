055a3d541b9a253bfc19de7b58b05380e9ca42c1
blinkfox <chenjiayin1990@163.com>
2017-05-30 16:28:47 +0800

    新建了adept的孵化项目

---
+++ src/main/java/com/blinkfox/hatch/adept/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.blinkfox.hatch.adept;
+
+import org.pmw.tinylog.Logger;
+
+/**
+ * HelloWorld.
+ * Created by blinkfox on 2017/5/30.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Logger.info("您好，Adept!");
+    }
+
+}
